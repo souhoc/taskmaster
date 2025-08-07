5122b7c6e6a6e1d9ad83f0b9731caa796cb5994d
sou.hoc <souhoc@icloud.com>
2025-08-07 12:18:01 +0200

    feat: autostart tasks

--- cmd/taskmaster/main.go
+++ cmd/taskmaster/main.go
@@ -46,6 +46,7 @@ func main() {
 	}
 
 	service := taskmaster.New(&cfg, taskmaster.WithOutputFile(logFile))
+	service.AutoStart()
 
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
