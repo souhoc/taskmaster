2f8e6947c89672e31555802ac774a16d16cac02a
sou.hoc <souhoc@icloud.com>
2025-08-08 17:37:32 +0200

    feat(util): spinner structure

---
+++ util/spinner.go
@@ -0,0 +1,48 @@
+package util
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+type Spinner struct {
+	chars []rune
+	done  chan struct{}
+	f     *os.File
+}
+
+// NewSinner return a Spinner.
+// If f is nil, it's set to os.Stdout.
+func NewSinner(f *os.File) *Spinner {
+	if f == nil {
+		f = os.Stdout
+	}
+	return &Spinner{
+		chars: []rune{'|', '/', '—', '\\'},
+		done:  make(chan struct{}, 1),
+		f:     f,
+	}
+}
+
+func (s *Spinner) Done() {
+	s.done <- struct{}{}
+	fmt.Fprintln(s.f)
+}
+
+func (s *Spinner) Spin(msg string) {
+	idx := 0
+
+	for {
+		select {
+		case <-s.done:
+			return
+		default:
+			fmt.Fprintf(s.f, "\r\033[2K%c %s", s.chars[idx], msg)
+			s.f.Sync()
+			idx = (idx + 1) % len(s.chars)
+			time.Sleep(time.Millisecond * 100)
+		}
+	}
+
+}
